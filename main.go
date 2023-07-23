7fb2f0026374b7726e39f2714cecc2fbd8960c3b
Karim Osman <kareemosman199@gmail.com>
2023-07-23 21:58:23 +0300

    porject outline

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type Node struct {
+}
+
+type Queue struct {
+}
+
+type Cache struct {
+	Queue Queue
+	Hash  Hash
+}
+
+type Hash map[string]*Node
+
+/**
+ *
+ * func to run a cache
+ **/
+func NewCache() Cache {
+	return Cache{Queue: NewQueue(), Hash: Hash{}}
+}
+
+/**
+ *
+ * func to create a queue
+ **/
+func NewQueue() Queue {
+
+}
+
+func main() {
+	fmt.Println("Start cache")
+	cache := NewCache()
+
+	for _, word := range []string{"parrot", "avocado", "tree", "text", "tomato", "tree"} {
+		cache.Check(word)
+		cache.Display()
+	}
 }
